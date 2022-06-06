bdd0a620cfd47fa9558c80e852911a28dd1bc100
kssrishi <92584456+kssrishi@users.noreply.github.com>
2022-06-06 17:29:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+noseX = 0
+noseY=0
 function preload() {
-    
+   mustage = loadImage(
+     "https://i.postimg.cc/xCdMFx9c/istockphoto-485318064-612x612-removebg-preview-2.png"
+   ); 
 }
 function setup() {
     canvas = createCanvas(300, 300)
@@ -12,6 +16,7 @@ function setup() {
 }
 function draw() {
     image(video,0,0,300,300)
+    image(mustage,noseX-35,noseY-10,70,50)
 }
 function take_snapshot() {
    save("clown.png") 
@@ -20,8 +25,10 @@ function modelLoaded() {
    console.log("pose net is intialized")
 }
 function gotPoses(results) {
-   if (result.length >0) {
+   if (results.length >0) {
       console.log(results);
+      noseX = results[0].pose.nose.x
+      noseY = results[0].pose.nose.y;
       console.log("nose x = " + results[0].pose.nose.x)
       console.log("nose y = " + results[0].pose.nose.y);
    }
